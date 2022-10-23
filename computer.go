937cc32add4032dff45d2fc2ee9de854da1613e9
Erik Engheim <erik.engheim@mac.com>
2022-10-24 00:03:09 +0200

    Code thus far
    Just hammering away on the simulator and disassembler code

--- computer.go
+++ computer.go
@@ -7,28 +7,8 @@ import (
 
 // "bufio"
 
-var AllInputRead = errors.New("All inputs read")
-
-type Instruction struct {
-	opcode Opcode
-	dst    uint8
-	addr   uint8
-	src    uint8
-	offset uint8
-}
-
-// Split a number from 0-9999 into its individual parts
-func decodeInstruction(instruction uint16) Instruction {
-	operands := instruction % 1000
-	addr := uint8(operands % 100)
-	return Instruction{
-		opcode: Opcode(instruction / 1000),
-		dst:    uint8(operands / 100),
-		addr:   addr,
-		src:    uint8(addr / 10),
-		offset: addr % 10,
-	}
-}
+var ErrAllInputRead = errors.New("all inputs read")
+var ErrProgramHalt = errors.New("reach halt instruction")
 
 type Computer struct {
 	PC        uint8      // Program counter 0-99
@@ -43,7 +23,29 @@ type Computer struct {
 }
 
 func (comp *Computer) Step() error {
+	pc := comp.PC
+	ir := comp.Memory[pc]
+	err := comp.ExecuteInstruction(ir)
+	if err != nil {
+		return fmt.Errorf("unable to execute instruction at address %d because: %w", pc, err)
+	}
 
+	// Make sure we didn't execute a branch instruction before updating Program counter
+	if pc == comp.PC {
+		comp.PC += 1
+	}
+
+	return nil
+}
+
+func (comp *Computer) RunSteps(nsteps int) error {
+	for i := 0; i < nsteps; i++ {
+		comp.PrintCurrentInstruction()
+		err := comp.Step()
+		if err != nil {
+			return fmt.Errorf("could not run program because %w", err)
+		}
+	}
 	return nil
 }
 
@@ -54,63 +56,69 @@ func (comp *Computer) ExecuteInstruction(instruction uint16) error {
 
 	regs := comp.Registers
 
-	opcode := Opcode(instruction / 1000)
-	operands := instruction % 1000
-	dst := uint8(operands / 100)
-	addr := uint8(operands % 100)
-	src := uint8(addr / 10)
-	offset := addr % 10
+	inst := decodeInstruction(instruction)
+
 	var rd uint8
 
-	if dst >= 1 && dst <= 9 {
-		rd = regs[dst]
+	if inst.dst >= 1 && inst.dst <= 9 {
+		rd = regs[inst.dst]
 	}
 
-	switch opcode {
+	switch inst.opcode {
 	case ADD:
-		rd = 100 % (regs[src] + regs[offset])
+		rd = 100 % (regs[inst.src] + regs[inst.offset])
 	case SUB:
-		rd = 100 % (regs[src] - regs[offset])
+		rd = 100 % (regs[inst.src] - regs[inst.offset])
 	case SUBI:
-		rd = 100 % (regs[src] - offset)
+		rd = 100 % (regs[inst.src] - inst.offset)
 	case LSH:
-		rd = 100 % (regs[src]*10 ^ offset)
+		rd = 100 % (regs[inst.src]*10 ^ inst.offset)
 	case RSH:
-		rd = regs[src] % (10 ^ offset)
-		regs[src] = regs[src] / (10 ^ offset)
+		rd = regs[inst.src] % (10 ^ inst.offset)
+		regs[inst.src] = regs[inst.src] / (10 ^ inst.offset)
 	case BRZ:
 		if rd == 0 {
-			comp.PC = addr
+			comp.PC = inst.addr
 		}
 	case BGT:
 		if rd > 0 {
-			comp.PC = addr
+			comp.PC = inst.addr
 		}
 	case LD:
-		if addr < 90 {
-			rd = uint8(comp.Memory[addr+1])
-		} else if addr == 90 {
+		if inst.addr < 90 {
+			rd = uint8(comp.Memory[inst.addr+1])
+		} else if inst.addr == 90 {
 			if comp.inpos >= len(comp.Inputs) {
-				return AllInputRead
+				return ErrAllInputRead
 			}
 		}
 	case ST:
-		if addr < 90 {
-			comp.Memory[addr+1] = uint16(rd)
-		} else if addr == 91 {
+		if inst.addr < 90 {
+			comp.Memory[inst.addr+1] = uint16(rd)
+		} else if inst.addr == 91 {
 			comp.Outputs = append(comp.Outputs, rd)
 		} else {
-			return fmt.Errorf("Writing to address %d is not supported in this version", addr)
+			return fmt.Errorf("writing to address %d is not supported in this version", inst.addr)
 		}
 	case HLT:
-		return nil
+		return ErrProgramHalt
 	default:
-		return fmt.Errorf("Opcode %d, is not supported. Must be between 0-9", opcode)
+		return fmt.Errorf("opcode %d, is not supported. Must be between 0-9", inst.opcode)
 	}
 
-	if dst >= 1 && dst <= 9 {
-		regs[dst] = rd
+	if inst.dst >= 1 && inst.dst <= 9 {
+		regs[inst.dst] = rd
 	}
 
 	return nil
+}
+
+// Print instruction at program counter disassembled with address and machine code
+// Example:
+//
+//	02: 1432; ADD  x4, x3, x2
+func (comp *Computer) PrintCurrentInstruction() {
+	machinecode := comp.Memory[comp.PC]
+	instruction := decodeInstruction(machinecode)
+	fmt.Printf("%d02: %d04; %v\n", comp.PC, machinecode, instruction)
 }
--- disassembler.go
+++ disassembler.go
@@ -1,1 +1,41 @@
 package calcutron
+
+import "fmt"
+
+type Instruction struct {
+	opcode Opcode
+	dst    uint8
+	addr   uint8
+	src    uint8
+	offset uint8
+}
+
+// Split a number from 0-9999 into its individual parts
+func decodeInstruction(instruction uint16) Instruction {
+	operands := instruction % 1000
+	addr := uint8(operands % 100)
+	return Instruction{
+		opcode: Opcode(instruction / 1000),
+		dst:    uint8(operands / 100),
+		addr:   addr,
+		src:    uint8(addr / 10),
+		offset: addr % 10,
+	}
+}
+
+// Disassembles instruction
+func (inst *Instruction) String() string {
+	switch inst.opcode {
+	case ADD, SUB:
+		return fmt.Sprintf("%4v x%d, x%d, x%d", inst.opcode, inst.dst, inst.src, inst.offset)
+	case SUBI, LSH, RSH:
+		return fmt.Sprintf("%4v x%d, x%d, %d", inst.opcode, inst.dst, inst.src, inst.offset)
+	case LD, ST, BRZ, BGT:
+		return fmt.Sprintf("%4v x%d, %d", inst.opcode, inst.dst, inst.addr)
+	case HLT:
+		return fmt.Sprintf("%4v", inst.opcode)
+	default:
+		break
+	}
+	return ""
+}
