0028943fe8943582f53827ce4a17f71d9edd0a6e
Erik Engheim <erik.engheim@mac.com>
2022-12-10 19:23:21 +0100

    Allow reading machine code files without newline between instructions

--- disasm/disassembler.go
+++ disasm/disassembler.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"unicode"
 
 	"github.com/ordovician/calcutron/prog"
 	"github.com/ordovician/calcutron/utils"
@@ -25,23 +26,38 @@ func DisassembleInstruction(machinecode uint) prog.Instruction {
 // Disassemble a machine code program read from reader
 func Disassemble(reader io.Reader) (*prog.Program, error) {
 	machineprogram := make([]uint, 0, 10)
-	scanner := bufio.NewScanner(reader)
+	builder := strings.Builder{}
+	bufReader := bufio.NewReader(reader)
 
-	for lineNo := 1; scanner.Scan(); lineNo++ {
-		line := strings.TrimSpace(scanner.Text())
-		if !utils.AllDigits(line) {
-			return nil, fmt.Errorf("machine code must be all digits. Cannot disassemble '%s'", line)
-		}
-		machinecode, err := strconv.Atoi(line)
+	for {
+		rune, _, err := bufReader.ReadRune()
+
 		if err != nil {
-			return nil, fmt.Errorf("%d: unable to disassemble because: %w", lineNo, err)
+			if err == io.EOF {
+				break
+			}
+			return nil, fmt.Errorf("unable to disassemble file: %w", err)
+		}
+
+		if unicode.IsSpace(rune) {
+			continue
+		}
+		if !unicode.IsDigit(rune) {
+			return nil, fmt.Errorf("machine code must be all digits. Cannot disassemble '%s'", string(rune))
+		}
+
+		builder.WriteRune(rune)
+		if builder.Len() < 4 {
+			continue
+		}
+
+		machinecode, err := strconv.Atoi(builder.String())
+		if err != nil {
+			return nil, fmt.Errorf("unable to disassemble because: %w", err)
 		}
 		machineprogram = append(machineprogram, uint(machinecode))
+		builder.Reset()
 	}
-	if scanner.Err() != nil {
-		return nil, fmt.Errorf("unable to disassemble file: %w", scanner.Err())
-	}
-
 	return DisassembleMemory(machineprogram)
 }
 
